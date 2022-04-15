2aab2baf220e73a1511e1f8f30a1a73fc43d3e2e
LautaZap <lautarogzapata@gmail.com>
2022-04-15 17:09:34 -0300

    created base grid 50*30

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let numRows = 50;
+  let numCols = 30;
+
+  const emptyGrid = () => {
+    let newGrid = [];
+    for (let i = 0; i < numRows; i++) {
+      const newCol = [];
+      for (let j = 0; j < numCols; j++) {
+        newCol.push(0);
+      }
+      newGrid.push(newCol);
+    }
+    return newGrid;
+  };
+
+  const [grid, setGrid] = useState(emptyGrid);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+        gridGap: "10px",
+        margin: "10px",
+      }}
+    >
+      {grid.map((rows, i) => {
+        return rows.map((cell, j) => {
+          return (
+            <div
+              style={{
+                border: "solid 1px black",
+                borderRadius: "100%",
+                width: 20,
+                height: 20,
+              }}
+              key={`${i}-${j}`}
+              onClick={() => {
+                console.log("click");
+              }}
+            />
+          );
+        });
+      })}
     </div>
   );
 }
